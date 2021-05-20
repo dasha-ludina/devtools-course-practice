238ef96688be09f7bfc0f39dfa0e37d6158c2a03
dasha-ludina <zxsa098721@gmail.com>
2021-05-20 13:58:08 +0300

    fix_4

--- modules/mortgage/test/test_mortgage_app.cpp
+++ modules/mortgage/test/test_mortgage_app.cpp
@@ -17,21 +17,21 @@ using std::string;
 class MortgageCalculatorTest : public ::testing::Test {
  protected:
 void Act(vector<string> args_) {
-    vector<const char*> options;
+  vector<const char*> options;
 
-    options.push_back("appname");
-    for (size_t i = 0; i < args_.size(); ++i) {
-      options.push_back(args_[i].c_str());
-    }
+  options.push_back("appname");
+  for (size_t i = 0; i < args_.size(); ++i) {
+    options.push_back(args_[i].c_str());
+  }
 
-    const char** argv = &options.front();
-    int argc = static_cast<int>(args_.size()) + 1;
+  const char** argv = &options.front();
+  int argc = static_cast<int>(args_.size()) + 1;
 
-    output_ = app_(argc, argv);
+  output_ = app_(argc, argv);
   }
 
   void Assert(std::string expected) {
-    ASSERT_TRUE(RE::PartialMatch(output_, RE(expected)));
+  ASSERT_TRUE(RE::PartialMatch(output_, RE(expected)));
   }
 
  private:
@@ -61,6 +61,14 @@ TEST_F(MortgageCalculatorTest, Can_Detect_Wrong_Number_Format) {
   Act(args);
 
   Assert("Wrong number format!.*");
+}
+
+TEST_F(MortgageCalculatorTest, Can_Interest_Rate) {
+  vector<string> args = { "1", "2", "2", "-4.0" };
+
+  Act(args);
+
+  Assert("Interest rate must be greater than zero!.*");
 }
 
 TEST_F(MortgageCalculatorTest, Can_Get_MonthlyPayment) {
